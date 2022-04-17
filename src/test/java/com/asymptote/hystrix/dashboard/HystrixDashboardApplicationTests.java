ae9a3ac250b645947320daed73c4cacbd8737862
Nishant Pathak <nishantpathak97@gmail.com>
2022-04-17 23:06:24 +0530

    adding project to git

---
+++ src/test/java/com/asymptote/hystrix/dashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asymptote.hystrix.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
